55dd0c0ab5bbcda52cc2cd5c2e81a2463dcd756a
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-01-24 23:01:54 -0800

    Create puzzle 5.js

---
+++ puzzle 5.js
@@ -0,0 +1,17 @@
+right
+right
+var firstColorSeen = getColor
+if (getColor == "blue") {down}
+if (getColor == "pink") {upn}
+right
+right
+var firstColorSeen = getColor
+if (getColor == "blue") {down}
+if (getColor == "pink") {upn}
+right
+right
+if (getColor == "blue") {down}
+if (getColor == "pink") {up}
+right
+right
+right
