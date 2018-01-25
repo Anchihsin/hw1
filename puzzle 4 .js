930603ca9cf7a8a7b592a30cf0da6deabda85bcd
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-01-24 22:49:37 -0800

    Create puzzle 4 .js

---
+++ puzzle 4 .js
@@ -0,0 +1,17 @@
+right
+down
+down
+down
+down
+var firstColorSeen = getColor
+down
+right
+setColor(firstColorSeen)
+right
+right
+up 
+var firstColorSeen = getColor
+down
+right
+setColor(firstColorSeen)
+right 
